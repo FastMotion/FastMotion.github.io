a885672acb1d073ee6632e28e2c1feaf42f6c3ab
Grigorii <Fastmotion1111@gmail.com>
2020-02-09 23:33:40 +0200

    refactor code (add test build version business card)

---
+++ project/Business-card/precache-manifest.f2bb6301a19aef0e9be4368e1f9e76e3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28604028838e290175b585fe733accbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16bb7cec4d0fbd80c6f8",
+    "url": "/static/css/2.e448224c.chunk.css"
+  },
+  {
+    "revision": "b840f7a79834c205c8ae",
+    "url": "/static/css/main.580670cc.chunk.css"
+  },
+  {
+    "revision": "16bb7cec4d0fbd80c6f8",
+    "url": "/static/js/2.990de84b.chunk.js"
+  },
+  {
+    "revision": "b840f7a79834c205c8ae",
+    "url": "/static/js/main.ec72f5d9.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  },
+  {
+    "revision": "653f2901d73984f72dd055f42b495ffa",
+    "url": "/static/media/201751.653f2901.jpg"
+  },
+  {
+    "revision": "fc8746c7e5c80106639776e93fc68a8c",
+    "url": "/static/media/512x512.fc8746c7.png"
+  },
+  {
+    "revision": "495faebfb4e22440cadadee0d4702122",
+    "url": "/static/media/abstract.495faebf.jpg"
+  },
+  {
+    "revision": "a1b1f1ddb0154a39e0ac9c61f27dbcaa",
+    "url": "/static/media/facebook.a1b1f1dd.png"
+  },
+  {
+    "revision": "ec46f34a97582e9783adbc29c376ad75",
+    "url": "/static/media/gmail.ec46f34a.png"
+  },
+  {
+    "revision": "c0044cf5967e29c12b6c9416f963232b",
+    "url": "/static/media/image2.c0044cf5.png"
+  },
+  {
+    "revision": "ab2335692318e4f81d0ee7509d2f87be",
+    "url": "/static/media/img.ab233569.jpg"
+  },
+  {
+    "revision": "47225f519ce0f4452c1c42fe2bddfc51",
+    "url": "/static/media/photo1.47225f51.PNG"
+  },
+  {
+    "revision": "105fb61bd893209b6051de2f98c9c115",
+    "url": "/static/media/photo2.105fb61b.jpg"
+  },
+  {
+    "revision": "c14768f4add68fb8f5ff5c8459af411c",
+    "url": "/static/media/telegram.c14768f4.png"
+  },
+  {
+    "revision": "5504d1dd8b39037139cd891270d83520",
+    "url": "/static/media/telephone.5504d1dd.png"
+  },
+  {
+    "revision": "88cc52a215d7a9ee84b016fc7bf3a460",
+    "url": "/static/media/Снимок1.88cc52a2.PNG"
+  }
+]);
